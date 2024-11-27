b55e2ad7eb3736448771fc17de272a3b56026de8
Oscar Davies <oscar@designlogica.co.uk>
2024-11-27 14:54:37 +0000

    Fix docker start

--- docker-start.sh
+++ docker-start.sh
@@ -11,8 +11,10 @@ cp -r -f ./versions /minecraft
 
 # Copy the server files
 cp ./eula.txt /minecraft
-cp ./server.properties /minecraft
+cp ./fabric.jar /minecraft
 
 # Start the server
 cd /minecraft
-java -jar -Xmx10G -Xms10G -Dfml.queryResult=confirm -jar forge-1.12.2-14.23.5.2859.jar nogui
+java -Xms10G -Xmx10G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar fabric.jar --nogui
+
+
