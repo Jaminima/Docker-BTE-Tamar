26c676556922de576ce741e6f781f8dc00c971f8
Oscar Davies <oscar@designlogica.co.uk>
2024-12-07 17:49:47 +0000

    Modify to depend on existing /minecraft folder

--- Dockerfile
+++ Dockerfile
@@ -10,24 +10,9 @@ RUN apt-get update && apt-get install -y \
 #Java Port
 EXPOSE 25565
 
-#------Configure Minecraft Server------
-
-#Create a directory for the server
-RUN mkdir /minecraft-init
-RUN chmod 777 /minecraft-init
-
-WORKDIR /minecraft-init
-
-#Download the forge files
-COPY ./forge/ /minecraft-init/
-
-#Run the server for the first time to generate the eula
-RUN java -Xmx1024M -Xms1024M -jar forge-1.12.2-14.23.5.2859.jar nogui
-
-#Accept the eula
-RUN echo "eula=true" > /minecraft-init/eula.txt
-
 #------Start The Server------
+RUN mkdir /minecraft-init
+WORKDIR /minecraft-init
 
 #Copy The Start Script
 COPY docker-start.sh ./docker-start.sh
--- docker-start.sh
+++ docker-start.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-cd /minecraft-init
+# cd /minecraft-init
 
-# Copy server folders to the server directory
-cp -r -f ./libraries /minecraft
-cp -r -f ./mods /minecraft
+# # Copy server folders to the server directory
+# cp -r -f ./libraries /minecraft
+# cp -r -f ./mods /minecraft
 
-# Copy the server files
-cp ./eula.txt /minecraft
-cp ./server.properties /minecraft
-cp ./*.jar /minecraft
+# # Copy the server files
+# cp ./eula.txt /minecraft
+# cp ./server.properties /minecraft
+# cp ./*.jar /minecraft
 
 # Start the server
 cd /minecraft
-java -jar -Xmx10G -Xms10G -Dfml.queryResult=confirm -jar forge-1.12.2-14.23.5.2859.jar nogui
+java -jar -Xmx5G -Xms5G -Dfml.queryResult=confirm -jar forge-1.12.2-14.23.5.2859.jar nogui
